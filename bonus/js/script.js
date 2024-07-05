444b5ea20f152a66b3b4b4cc6c7518572f5807a6
Corfaya <lauracorrado0696@gmail.com>
2024-07-05 16:07:35 +0200

    [UPD] parameter add to function createDivSquare and add it as a class of the current element created

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,21 +1,23 @@
 // Function
-function createDivSquare() {
+function createDivSquare(diff) { //NEW: added parameter
     let currentElement = document.createElement("div")
     currentElement.classList.add("square")
+    currentElement.classList.add(diff) //NEW parameter as second class
     return currentElement;
 }
 
 // elements recovery
 const grid = document.getElementById("grid")
 const btn = document.getElementById("play")
-const level = document.getElementById("level")
+
 
 // click event on btn
 btn.addEventListener("click", function() {
     grid.innerHTML = ""
     let square;
-    for (let k = 0; k < 100; k++) {
-        square = createDivSquare();
+
+    for (let k = 0; k < 100; k++) { //to replace end loop condition with squares length
+        square = createDivSquare(squareClass);
         square.addEventListener("click", function() {
             this.classList.add("clicked-azure")
             console.log("Hai clickato su " + (k + 1))
